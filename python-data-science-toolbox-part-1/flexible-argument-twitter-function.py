ff9e16866ce897bfe72d3501cb389e07a2e48c6a
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-03-27 19:35:35 +0100

    A function that counts how many tweets are in certain languages with a default argument

---
+++ python-data-science-toolbox-part-1/flexible-argument-twitter-function.py
@@ -0,0 +1,40 @@
+# A function that counts how many tweets are in certain languages
+# pandas has been imported as pd and the 'tweets.csv' file has been imported into the DataFrame tweets_df. Parts of the code from your previous work are also provided.
+# 
+# import pandas as pd
+# tweets_df = pd.read_csv('tweets.csv')
+
+# Define count_entries()
+def count_entries(df, col_name = 'lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+
+    # Extract column from DataFrame: col
+    col = df[col_name]
+    
+    # Iterate over the column in DataFrame
+    for entry in col:
+
+        # If entry is in cols_count, add 1
+        if entry in cols_count.keys():
+            cols_count[entry] += 1
+
+        # Else add the entry to cols_count, set the value to 1
+        else:
+            cols_count[entry] = 1
+
+    # Return the cols_count dictionary
+    return cols_count
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df)
+
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'source')
+
+# Print result1 and result2
+print(result1)
+print(result2)
