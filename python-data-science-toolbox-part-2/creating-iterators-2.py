0ad54e0b12c63fd9446e04e4760ebe3d5b6ab46e
fejesgergorichard <fejesgergorichard@gmail.com>
2020-04-03 20:47:57 +0200

    iterators

---
+++ python-data-science-toolbox-part-2/creating-iterators-2.py
@@ -0,0 +1,26 @@
+# range() doesn't actually create the list; instead, it creates a range object with an iterator that produces
+# the values until it reaches the limit (in the example, until the value 4). If range() created the actual list,
+# calling it with a value of 10100 may not work
+
+# Create an iterator for range(3): small_value
+small_value = iter(range(3))
+
+# Print the values in small_value
+print(next(small_value))
+print(next(small_value))
+print(next(small_value))
+
+# Loop over range(3) and print the values
+for num in range(3):
+    print(num)
+
+
+# Create an iterator for range(10 ** 100): googol
+googol = iter(range(10 ** 100))
+
+# Print the first 5 values from googol
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
