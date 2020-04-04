fbf368a1e60389638649ab24a563c24154a9a859
fejesgergorichard <fejesgergorichard@gmail.com>
2020-04-04 14:46:36 +0200

    filter() function description

--- python-data-science-toolbox-part-1/lambda-4.py
+++ python-data-science-toolbox-part-1/lambda-4.py
@@ -2,6 +2,8 @@
 fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
 
 # Use filter() to apply a lambda function over fellowship: result
+# The filter() function takes two arguments: a function and a list
+# This function returns a filtered list with the elements that were evaluated 'True' by the passed function
 result = filter(lambda a: len(a) > 6, fellowship)
 
 # Convert result to a list: result_list
