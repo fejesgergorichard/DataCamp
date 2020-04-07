969c4c9d818602a8054e6a6a0da759bd674d9eb8
fejesgergorichard <fejesgergorichard@gmail.com>
2020-04-07 19:15:08 +0200

    FizzBuzz Leetcode - made it prettier

--- python-data-science-toolbox-part-2/fizzbuzz.py
+++ python-data-science-toolbox-part-2/fizzbuzz.py
@@ -1,2 +1,8 @@
-set = [str(i) if i % 3 != 0 and i % 5 != 0 else 'fizzBuzz' if i % 15 == 0 else 'fizz' if i % 3 == 0 else 'buzz' for i in range(1,101)]
+# Description:
+# Write a program that outputs the string representation of numbers from 1 to n.
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. 
+# For numbers which are multiples of both three and five output “FizzBuzz”.
+# Example output: ["1", "2", "Fizz", "4", "Buzz", "Fizz" ...]
+
+set = ['fizzBuzz' if i % 15 == 0 else 'fizz' if i % 3 == 0 else 'buzz' if i % 5 == 0 else str(i) for i in range(1,101)]
 print(set)
