af65e3d490e63d830ec635a27f15ca8d847f06fb
fejesgergorichard <fejesgergorichard@gmail.com>
2020-04-08 19:20:19 +0200

    Added conditional in the list comprehension

--- python-data-science-toolbox-part-2/list-comprehensions-for-time-stamped-data.py
+++ python-data-science-toolbox-part-2/list-comprehensions-for-time-stamped-data.py
@@ -6,7 +6,7 @@
 tweet_time = df['created_at']
 
 # Extract the clock time: tweet_clock_time
-tweet_clock_time = [entry[11:19] for entry in tweet_time]
+tweet_clock_time = [entry[11:19] for entry in tweet_time if entry[17:19] == '19']
 
 # Print the extracted times
 print(tweet_clock_time)
