c10a9bd38a46e2cbaa122c904d1e39f31ce1b7ee
fejesgergorichard <fejesgergorichard@gmail.com>
2020-04-14 23:08:01 +0200

    using ecdf-function-1.py and plotting the data

---
+++ statistical-thinking-in-python-part-1/ecdf-function-2.py
@@ -0,0 +1,13 @@
+# Compute ECDF for versicolor data: x_vers, y_vers
+x_vers, y_vers = ecdf(versicolor_petal_length)
+
+# Generate plot
+_ = plt.plot(x_vers, y_vers, marker = '.', linestyle = 'none')
+
+# Label the axes
+_ = plt.xlabel('Range')
+_ = plt.ylabel('ECDF')
+
+
+# Display the plot
+plt.show()
