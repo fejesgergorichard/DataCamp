26ed74b5ede19f868ef716375040038b1c258726
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-04-24 11:49:38 +0200

    Perform bernoulli trials on randomly generated datasets

---
+++ statistical-thinking-in-python-part-1/hacker-statistics-3.py
@@ -0,0 +1,19 @@
+# Seed random number generator
+np.random.seed(42)
+
+# Initialize the number of defaults: n_defaults
+n_defaults = np.empty(1000)
+
+# Compute the number of defaults on a loan with the probability of 0.05.
+# We take 1000 samples and perform the bernoulli trials on 100 random cases.
+for i in range(1000):
+    n_defaults[i] = perform_bernoulli_trials(100, 0.05)
+
+
+# Plot the histogram with default number of bins; label your axes
+_ = plt.hist(n_defaults, normed = True)
+_ = plt.xlabel('number of defaults out of 100 loans')
+_ = plt.ylabel('probability')
+
+# Show the plot
+plt.show()
